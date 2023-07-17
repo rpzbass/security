a203315abd5ef6c07c71638ff301b18900938e02
rpzbass <76059593+rpzbass@users.noreply.github.com>
2023-07-17 20:17:11 -0300

    Update listarIP.bat

--- scripts/Utils/listarIP.bat
+++ scripts/Utils/listarIP.bat
@@ -7,7 +7,7 @@ for /F "tokens=2 delims=:" %%i in ('ipconfig ^| find "IPv4" ^| find "1."') do se
 
 set HOST=%computername%
 
-msg  /server:192.168.1.65 * IP : %IP%  MAQUINA : %HOST% 
+msg  /server:10.0.0.2 * IP : %IP%  MAQUINA : %HOST% 
 pause
 
 
